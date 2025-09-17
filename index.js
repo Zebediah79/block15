727ffc4dbc262b86176b0ded97076954020e096c
Zebediah Allton <zebediah.allton@gmail.com>
2025-09-16 19:37:40 -0500

    Finished all objectives within the rubric.

--- index.js
+++ index.js
@@ -1,22 +1,28 @@
-// /**
-//  * @param {string} commaSeparatedFlavors
-//  * @returns {number[]} the string converted into an array of numbers
-//  */
+// Code taken from block 14 string to number conversion function and used
+// for creating array of flavors to to loop through and create an object
+// that adds a property if not available or increments a property if it
+// already exists.
+
+/**
+ * @param {string} commaSeparatedFlavors
+ * @returns {flavors} the string converted into an object of flavors
+ */
 
 function convertStringToFlavors(commaSeparatedFlavors) {
-  const differentFlavors = commaSeparatedFlavors.split(",");
-  console.log(differentFlavors);
-  const flavors = {};
+  const differentFlavors = commaSeparatedFlavors.split(","); // creates array of flavors from prompt
+  const flavors = {}; //initialize object flavors with zero properties.
+
   for (let f in differentFlavors) {
-    const flavType = differentFlavors[f];
-    console.log(flavType);
-    if ("flavType" in flavors) {
-      flavors[flavType] += 1;
+    // loop through array of differentFlavors
+    const flavorType = differentFlavors[f]; //stores array element in variable flavorType
+    if (flavorType in flavors) {
+      //checks if flavorType from array element exists as an object property
+      flavors[flavorType] += 1; // if it exists, increment by one.
     } else {
-      flavors[flavType] = 1;
+      flavors[flavorType] = 1; // if it doesn't exist, add object property and initialize to 1.
     }
   }
-  return flavors;
+  return flavors; // return object flavors with all properties and total number of each one.
 }
 
 function describeFlavors(flavor) {
